d996aca4dbc8d84fff4ed1bb33204d294404a516
amirreza <shahidiam83@gmail.com>
2022-08-16 13:19:18 +0430

    add react router file

---
+++ resources/js/router.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+function App(){
+	return(
+		<div>
+		</div>
+		);
+
+}
+
+export default App;
+ReactDOM.render(<App/>,document.getElementById('index'));
