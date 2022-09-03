1f5f65de00d3c3cde22ba2bec35713313b4b4b5b
amirreza <shahidiam83@gmail.com>
2022-09-03 10:56:47 +0430

    add basic psycho test methods

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Test;
-
+use App\Imports\TestsImport;
+use Maatwebsite\Excel\Facades\Excel;
 
 class TestController extends Controller
 {
@@ -14,8 +15,32 @@ class TestController extends Controller
     }
 
 
+    public function getQuestions($test_id){
+        $test = Test::where('id',$test_id)->first();
 
-    public function list(Request $request){
+        $questions_file = Storage::disk('local')->get($test->questions);
+
+        $data = Excel::toArray(new TestsImport(),$questions_file);
+
+        $rows = array();
+        $questions = array();
+
+        foreach($rows as $row){
+            array_push($questions,$row);
+        }
+
+        return response()->json($questions,200);
+
+    }
+
+    public function confirm(Request $request){
+
+    }
+
+    public function list(){
+        return response()->json(
+            Test::all()->get()
+            ,200);
     }
 
 
